ddf12e8a85b857eb1be7bc28764ace64795ac2d6
Rick Meneely <richard@getshogun.com>
2023-05-15 16:14:05 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # git-matching-commits
-GitHub Action to return a list of matching commits from a tag to the branch HEAD
+This GitHub Action returns a comma separated string of matching git commit SHAs.
+
+## Usage
+```yaml
+    - uses: rmeneely/git-matching-commits@v1
+      with:
+        start_tag_pattern: regex
+        end_tag_pattern: regex
+        commit_message_pattern: regex
+```
+
+### Inputs
+All inputs are optional. If not set the default value will be used.
+
+| Name                   | Description                                 | Default              |
+| ---------------------- |:------------------------------------------- | :--------------------|
+| start_tag_pattern      | regex expression to match the starting tag  | v[0-9]*.[0-9]*.[0-9] |
+| end_tag_pattern        | regex expression to match the ending tag    | HEAD |
+| commit_message_pattern | regex expression to match a returned commit | .* |
+
+
+## Examples
+```yaml
+    # Returns a list of commits between the latest commit with tag v*.*.0 and the current branch HEAD which commit messages contain '[Hotfix]'
+    - uses: rmeneely/git-matching-commits@v1
+      with:
+        start_tag_pattern: 'v[0-9]*.[0-9]*.0'
+        commit_message_pattern: '\[Hotfix\]'
+```
+
+
+## Output
+```shell
+steps.git-matching-commits.outputs.commits - Set to comma separated list of matched commit SHAs
+```
+
+## License
+The MIT License (MIT)
---
+++ git-matching-commits.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Modules
+import argparse
+import re
+import os
+import subprocess
+from git import Repo
+
+# Default values
+DefaultStartTagPattern = 'v[0-9]+.[0-9]+.[0-9]+'
+DefaultEndTagPattern = 'HEAD'
+DefaultCommitMessagePattern = '.*'
+
+# Arguments
+parser = argparse.ArgumentParser(description='Match Git commits based on matching commit message')
+parser.add_argument('--start_tag_pattern', type=str, help='Start tag pattern')
+parser.add_argument('--end_tag_pattern', type=str, help='End tag pattern')
+parser.add_argument('--commit_message_pattern', type=str, help='Commit message pattern')
+args = parser.parse_args()
+
+# Global variables
+StartTagPattern = args.start_tag_pattern if (args.start_tag_pattern != None) else DefaultStartTagPattern
+EndTagPattern = args.end_tag_pattern if (args.end_tag_pattern != None) else DefaultEndTagPattern
+CommitMessagePattern = args.commit_message_pattern if (args.commit_message_pattern != None) else DefaultCommitMessagePattern
+
+# Path to the Git repository
+repo_path = '.'
+
+# Open the Git repository
+repo = Repo(repo_path)
+git = repo.git
+
+# Get most recent matching tags
+start_tag = git.tag('--sort=committerdate', '--list', '{0}'.format(StartTagPattern))
+end_tag = 'HEAD'
+if EndTagPattern != 'HEAD':
+    end_tag = git.tag('--sort=committerdate', '--list', '{0}'.format(EndTagPattern))
+
+# Get all commits between the two tags (not including the start tag)
+commits = list(repo.iter_commits("{0}..{1}".format(start_tag, end_tag)))
+matched_commits = []
+for commit in commits:
+    if re.search(CommitMessagePattern, commit.message):
+        matched_commits.append(commit.hexsha)
+
+# Return matching commits
+print("commits={}".format(','.join(matched_commits)))
+# os.environ['COMMITS'] = ','.join(matched_commits)
+
+# End of file
