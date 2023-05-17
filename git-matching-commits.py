622a054e3d184af8818ff6116f16ff3e916fa6c6
Rick Meneely <richard@getshogun.com>
2023-05-16 19:46:31 -0700

    Debugging

--- git-matching-commits.py
+++ git-matching-commits.py
@@ -50,7 +50,7 @@ if Debug:
     print("CommitMessagePattern={0}".format(CommitMessagePattern))
 
 # Get all commits between the two tags (not including the start tag)
-commits = git.rev_list("{0}..{1}".format(start_tag, end_tag), reverse=True).split('\n')
+commits = git.rev_list("{0}..{1}".format(start_tag, end_tag)).split('\n')
 # commits = list(repo.iter_commits("{0}..{1}".format(start_commit.hexsha, head.hexsha), reverse=True))
 if Debug:
     all_commits = []
@@ -58,9 +58,9 @@ if Debug:
     for commit in commits:
         print("commit={0}".format(commit))
         all_commits.append(commit)
-        message = git.commit.message(commit)
-        if re.search(CommitMessagePattern, message):
-            matched_commits.append(commit)
+        # message = git.commit.message(commit)
+        # if re.search(CommitMessagePattern, message):
+        #     matched_commits.append(commit)
         # print("commit={0}".format(commit.hexsha))
         # print("commit.message={0}".format(commit.message))
         # all_commits.append(commit.hexsha)
